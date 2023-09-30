6861c551dab14a9cd98eb918f92939fa7bf0ee94
manoj123y <113856419+manoj123y@users.noreply.github.com>
2023-09-30 10:16:52 +0530

    Add files via upload

---
+++ src/simplecalculator1/Simplecalculator1.java
@@ -0,0 +1,24 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package simplecalculator1;
+
+/**
+ *
+ * @author Manoj
+ */
+public class Simplecalculator1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        calculator cu=new calculator();
+        cu.setVisible(true);
+    }
+    
+}
